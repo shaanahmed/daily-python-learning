0b2c3280cc362433f93a8e8917152af21b84e71d
Shaan Ahmed <shaanahmed003@gmail.com>
2026-02-02 16:50:24 +0700

    Added my Bio

---
+++ Day1 (Basic Python)/my_print_details.py
@@ -0,0 +1,9 @@
+print([["Hi my name is Shaan Ahmed."],
+["I am learning Python to become a Mainframe Developer and Data Scientist."],
+["I will automate things for my easy workflow with python so my day to day life becomes easy."],
+["I have a Bachelor's degree in Bachelor in Computer Application (AI and Data Sciennce[IBM])"], 
+["Studied at CT University, Ludhiana [Batch 2022 - 2025]."],
+["Completed 6 months intership at Pisoft Informatics learned nothing."],
+["Tried music Production released many songs but not a guaranteed way to earn consistent money."],
+["I always wanted to be like Tony Stark, the reason why I choose AI in my degree."],
+["So working hard again and this time no distraction nothing and pure focus on earning minimumm 150k dollar salary per annum with my Laptop"]])
