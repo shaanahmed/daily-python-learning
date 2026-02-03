76efd15767c2aa5a16545409190fdbbcb0b9a3a6
Shaan Ahmed <shaanahmed003@gmail.com>
2026-02-03 13:40:35 +0700

    Added Boolean Logic Problems Practice

---
+++ Day2_Problem_Solving/Boolean_Logic.py
@@ -0,0 +1,11 @@
+# %% 1. The "Empty Check" (Conversion to Boolean)
+"""Goal: Understand which values Python considers "Empty" or False.
+Task: Create a program that takes a string input. Use the bool() function to convert that input to a Boolean.
+Challenge: Run the program twice. Once, type something (e.g., "Vietnam") and see the result. The second time, just press Enter without typing anything.
+Logic: This helps you understand "Falsy" values in Python."""
+
+user = input("Enter anything: ")
+print(bool(user))
+
+
+# %%
