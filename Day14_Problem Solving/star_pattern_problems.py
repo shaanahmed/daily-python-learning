f746aff8e71c1aaae95867c5133bf1c71bf2f648
Shaan Ahmed <shaanahmed003@gmail.com>
2026-02-15 21:58:46 +0700

    added more star pattern problems

--- Day14_Problem Solving/star_pattern_problems.py
+++ Day14_Problem Solving/star_pattern_problems.py
@@ -45,4 +45,82 @@ for i in range(1, rows+1):
 for i in range(rows, 0, -1):
     print(' ' * (rows - i) + '*' * (2 * i - 1))
 
+# %% Pattern 4: Butterfly
+
+"""
+*        *
+**      **
+***    ***
+****  ****
+**********
+****  ****
+***    ***
+**      **
+*        *
+"""
+
+rows = 5
+#upper part
+for i in range(1, rows + 1):
+    print('*' * i + ' ' * (2 * (rows - i)) + '*' * i)
+#lower part
+for i in range(rows, 0, -1):
+    print('*' * i + ' ' * (2 * (rows - i)) + '*' * i)
+
+
+# %% Pattern 5: Right Triangle
+"""
+*
+**
+***
+****
+*****
+"""
+rows = 5
+for i in range(1, rows+1):
+    print('*' * i)
+
+
+# %% Pattern 6: Inverted Right Triangle(Upside Down)
+"""
+*****
+****
+***
+**
+*
+"""
+rows = 5
+for i in range(rows, 0, -1):
+    print('*' * i)
+
+# %% Problem 7: Hollow square (or box)
+"""
+*****
+*   *
+*   *
+*   *
+*****
+
+"""
+rows = 5
+for i in range(rows):
+    if i == 0 or i == rows - 1:
+        print('*' * rows)
+    else:
+        print('*' + " " * (rows - 2) + '*')
+
+# %% Problem 8: 5x5 square of asterisks
+"""
+*****
+*****
+*****
+*****
+*****
+
+"""
+rows = 5
+for i in range(rows):
+    print('*' * rows)
+
+
 # %%
