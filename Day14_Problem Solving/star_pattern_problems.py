2c1d90beb5d9330192bb0eb71202613879600a95
Shaan Ahmed <shaanahmed003@gmail.com>
2026-02-16 20:38:52 +0700

    added some more star pattern problem

--- Day14_Problem Solving/star_pattern_problems.py
+++ Day14_Problem Solving/star_pattern_problems.py
@@ -123,4 +123,78 @@ for i in range(rows):
     print('*' * rows)
 
 
-# %%
+# %% Pattern 9: Hollow Diamond
+"""
+
+    *
+   * *
+  *   *
+ *     *
+  *   *
+   * *
+    *
+
+"""
+rows = 5
+#upper part
+for i in range(1, rows + 1):
+    print(' ' * (rows - i) + '*' + ' ' * (2 * i - 3) + ('*' if i > 1 else ''))
+#lower part
+for i in range(rows -1, 0, -1):
+    print(' ' * (rows - i) + '*' + ' ' * (2 * i - 3) + ('*' if i > 1 else ''))
+
+
+# %% Pattern 10: Hollow Pyramid
+"""
+
+    *
+   * *
+  *   *
+ *     *
+*********
+
+"""
+rows = 5
+for i in range(1, rows + 1):
+    if i == rows:
+        print('*' * (2 * i - 1))
+    else:
+        print(' ' * (rows - i) + '*' + ' ' * (2 * i - 3) + ('*' if i > 1 else ''))
+
+
+# %% Pattern 2: Left facing Right-Aligned Triangle
+"""
+    *
+   **
+  ***
+ ****
+*****
+
+"""
+
+rows = 5
+for i in range(1, rows+1):
+    print(' ' * (rows - i) + '*' * i)
+
+
+# %%Pattern 3: X Pattern
+"""
+
+*   *
+ * *
+  *
+ * *
+*   *
+
+"""
+
+rows = 5
+for i in range(1, rows + 1):
+    for j in range(1, rows + 1):
+        if i == j or i + j == rows + 1:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print()
+
+# %% 
