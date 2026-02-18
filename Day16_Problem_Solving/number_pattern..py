9dc8bf03bfa7f7b58316fcf6e469ad0dad03e612
Shaan Ahmed <shaanahmed003@gmail.com>
2026-02-18 11:28:53 +0700

    Added more number pattren problems

--- Day16_Problem_Solving/number_pattern..py
+++ Day16_Problem_Solving/number_pattern..py
@@ -34,4 +34,42 @@ for i in range(rows, 0, -1):
     print()
 
     
+# %% 3. Descending Column Pattern
+"""Instead of repeating the row number, this pattern counts down within the row itself.
+
+5 4 3 2 1 
+4 3 2 1 
+3 2 1 
+2 1 
+1
+
+"""
+
+rows = 5
+for i in range(rows, 0, -1):
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+    print()
+
+
+# %% 4. Floyd's Triangle (Incremental Numbers)
+"""This one is slightly different because the number doesn't "reset" at the start of a new
+row; it keeps growing.
+
+1 
+2 3 
+4 5 6 
+7 8 9 10
+
+"""
+
+rows = 4
+num = 1
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        print(num, end=" ")
+        num += 1
+    print()
+
+    
 # %%
