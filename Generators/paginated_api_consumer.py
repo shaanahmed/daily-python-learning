540048da40da0fa0fb83398552934fb01a90ac43
Shaan Ahmed <shaanahmed003@gmail.com>
2026-03-09 23:41:44 +0530

    Added a generator program

---
+++ Generators/paginated_api_consumer.py
@@ -0,0 +1,19 @@
+def count_to_infinity(start):
+    """A generator that counts up forever (without crashing your PC)."""
+    while True:
+        yield start  # This 'pauses' the function and sends the value out
+        start += 1
+
+# 2. Setup the generator
+counter = count_to_infinity(10)
+
+# 3. Execute instantly
+print(next(counter))  # Output: 10
+print(next(counter))  # Output: 11
+print(next(counter))  # Output: 12
+
+# You can also use it in a loop with a break
+for num in counter:
+    print(f"Looping: {num}")
+    if num >= 15: 
+        break
